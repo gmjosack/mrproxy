e3043dabe33afe0350100b5fc22a3f86c627a32a
Gary M. Josack <gary@byoteki.com>
2014-06-06 15:51:22 -0700

    Catch error when backend goes away.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 kwargs = {
     "name": "mrproxy",
-    "version": "0.1.1",
+    "version": "0.1.2",
     "scripts": ["bin/mrproxy"],
     "description": "Mediocre Reverse Proxy.",
     # PyPi, despite not parsing markdown, will prefer the README.md to the
